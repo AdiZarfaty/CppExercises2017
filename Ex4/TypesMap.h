43b295c664a18b49e17fbd8c437e429b4c00dd8f
Tomer Amir <tomer.a@hotmail.com>
2018-01-10 23:06:54 +0200

    TypesMap almost ready. Just need to finish the "get" function Also added the first main function for testing

--- Ex4/TypesMap.h
+++ Ex4/TypesMap.h
@@ -21,23 +21,33 @@ using std::endl;
 template <class Piece>
 class TypesMap {
 private:
-	map<string, std::vector<Piece>> _types;
+	map<string, std::vector<Piece*>> _types;
 
 public:
 	void addPiece(Piece * piece);
 
-	std::vector<Piece> get(Piece constraints);
+	std::vector<Piece*> get(Piece constraints);
 };
 
 template <class Piece>
 void TypesMap<Piece>::addPiece(Piece * piece) {
-	// TODO
+	auto type = piece->getType();
+
+	if (_types.find(type) == _types.end()) {
+		_types[type] = std::vector<Piece*>();
+	}
+
+	_types[type].push_back(piece);
 }
 
 template <class Piece>
-std::vector<Piece> TypesMap<Piece>::get(Piece constraints) {
-	cout << "here" << endl;
-	return std::vector<Piece>();
+std::vector<Piece*> TypesMap<Piece>::get(Piece constraints) {
+	// TODO
+	if (_types.empty()) {
+		return std::vector<Piece*>();
+	}
+
+	return _types.begin()->second;
 }
 
 #endif //EX4_TYPESMAP_H
--- Ex4/main.cpp
+++ Ex4/main.cpp
@@ -2,23 +2,15 @@
 #include <iostream>
 #include "Puzzle2dPiece.h"
 
-using std::list;
-using std::cout;
-using std::endl;
-
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-
-	Piece<7> p(1, 2, 3, 4, 5, 6, 7);
-
-	for (const auto &i: p) {
-		cout << i << endl;
-	}
-
 	std::list<Puzzle2dPiece<5>> pieces = {{0, 3, 2, -5}, {0, -2, 2, -5}};
-
 	auto groups = groupPuzzlePieces(pieces.begin(), pieces.end());
-	auto somePieces = groups.get({0, std::numeric_limits<int>::min(), 2, -5});
+	// note that there is no & on the auto below (was in previous version)
+	auto some_pieces = groups.get({0, std::numeric_limits<int>::min(), 2, -5});
+
+	for(auto piece_ptr : some_pieces) {
+		std::cout << *piece_ptr << std::endl; // will print both pieces!
+	}
 
 	return 0;
 }
