11746a99e39bf4c10ead82d13116334e9596bc10
Tomer Amir <tomer.a@hotmail.com>
2018-01-26 12:52:10 +0200

    Changed main to print something

--- Ex4/main.cpp
+++ Ex4/main.cpp
@@ -3,26 +3,26 @@
 #include "Puzzle2dPiece.h"
 #include "Puzzle3dPiece.h"
 
-//int main() {
-//	std::list<Puzzle2dPiece<5>> pieces = {{0, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 1}, {-5, 0, 0, 0}, {1, 0, 0, 1}};
-//	auto groups = groupPuzzlePieces(pieces.begin(), pieces.end());
-//	// note that there is no & on the auto below (was in previous version)
-//	auto some_pieces = groups.get({std::numeric_limits<int>::min(), 0, 0, std::numeric_limits<int>::min()});
-//
-//	for(auto piece_ptr : some_pieces) {
-//		std::cout << *piece_ptr << std::endl; // will print both pieces!
-//	}
-//
-//	return 0;
-//}
-
 int main() {
-	list<Puzzle3dPiece<1>> pieces = {{0, 1, 1, 1, -1, -1}, {0, -1, 1, 1, 1, 1}};
+	std::list<Puzzle2dPiece<5>> pieces = {{0, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 1}, {-5, 0, 0, 0}, {1, 0, 0, 1}};
 	auto groups = groupPuzzlePieces(pieces.begin(), pieces.end());
 	// note that there is no & on the auto below (was in previous version)
-	auto some_pieces = groups.get({0, 1, 1, 1, 1, 1});
+	auto some_pieces = groups.get({std::numeric_limits<int>::min(), 0, 0, std::numeric_limits<int>::min()});
+
 	for(auto piece_ptr : some_pieces) {
-		std::cout << *piece_ptr << std::endl; // will print nothing!
+		std::cout << *piece_ptr << std::endl; // will print both pieces!
 	}
+
+	return 0;
 }
+
+//int main() {
+//	list<Puzzle3dPiece<1>> pieces = {{0, 1, 1, 1, -1, -1}, {0, -1, 1, 1, 1, 1}};
+//	auto groups = groupPuzzlePieces(pieces.begin(), pieces.end());
+//	// note that there is no & on the auto below (was in previous version)
+//	auto some_pieces = groups.get({0, 1, 1, 1, 1, 1});
+//	for(auto piece_ptr : some_pieces) {
+//		std::cout << *piece_ptr << std::endl; // will print nothing!
+//	}
+//}
 
